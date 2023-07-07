e6828c5f19848509bdd7536867904856bebed92c
xingxingbao <2948651208@qq.com>
2023-07-07 18:19:28 +0800

    第一次修改

---
+++ src/test/java/com/example/gitpratice/GitPraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitpratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPraticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
